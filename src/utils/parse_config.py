a060c560e5d46a81ec7707f947f2dfa87dc4e6f5
Phat Tran <phat.tran.developer@gmail.com>
2025-07-04 21:48:06 +0700

    Update parse config

--- src/utils/parse_config.py
+++ src/utils/parse_config.py
@@ -25,9 +25,12 @@ def get_lvm_llamaparse() -> LlamaParse:
         use_vendor_multimodal_model=True,
         vendor_multimodal_model_name="anthropic-sonnet-3.5",
         parsing_instructions=(
-            "Extract all textual content in logical reading order, preserving structure and hierarchy "
-            "such as headings, sections, tables, and bullet points. If the document contains images, figures, "
-            "charts, diagrams, or other visual elements, describe them clearly in Vietnamese. Ensure that any "
-            "caption or inferred description related to non-text content is also written in Vietnamese."
+            "This is a book for Vietnamese high-school students, "
+            "written in Vietnamese. The text is structured with headings, sections, and other elements. "
+            "Your task is to extract the text content in a logical reading order, preserving the structure"
+            " and hierarchy such as headings, sections, tables, and bullet points. "
+            "If the document contains images, figures, charts, diagrams, or other visual elements,"
+            " describe them clearly in Vietnamese. Ensure that any caption or inferred description "
+            "related to non-text content is also written in Vietnamese. "
         ),
     )
