c463b55b934e5ec1a21972fa61e16e8a6e014558
Phat Tran <phat.tran.developer@gmail.com>
2025-07-10 20:56:51 +0700

    Add ReRanker

--- src/rag/base_rag.py
+++ src/rag/base_rag.py
@@ -9,7 +9,11 @@ from llama_index.core import (
     SimpleDirectoryReader,
 )
 from llama_index.core.tools import QueryEngineTool
-from llama_index.core.query_engine import SubQuestionQueryEngine
+from llama_index.core.query_engine import SubQuestionQueryEngine, RetrieverQueryEngine
+from llama_index.retrievers.bm25 import BM25Retriever
+from llama_index.core.retrievers import QueryFusionRetriever
+from llama_index.core.postprocessor import SimilarityPostprocessor
+from llama_index.core.node_parser import SentenceSplitter
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +24,7 @@ class BaseRAG(ABC):
         Initialize the RAG
         """
         self.llm = self.get_llm()
-        self.index = self.get_index()
+        self.query_engine = self.get_query_engine()
 
     @abstractmethod
     def get_llm(self) -> any:
@@ -30,9 +34,9 @@ class BaseRAG(ABC):
         raise NotImplementedError("Subclasses must implement this method")
 
     @abstractmethod
-    def get_index(self) -> any:
+    def get_query_engine(self) -> any:
         """
-        Get the index to use for the RAG.
+        Get the query engine to use for the RAG.
         """
         raise NotImplementedError("Subclasses must implement this method")
 
@@ -45,49 +49,51 @@ class BaseRAG(ABC):
 
 
 class LocalRAG(BaseRAG):
+    def __init__(
+        self, storage_dir: str = "src/.index_storage", data_dir: str = "../data"
+    ):
+        super().__init__()
+        self.storage_dir = storage_dir
+        self.data_dir = data_dir
+
     def get_llm(self) -> any:
         """
         Use default OpenAI model.
         """
         return OpenAI(model="gpt-4o-mini")
 
-    def get_index(self) -> any:
+    def get_query_engine(self) -> any:
         """
-        Load local index from storage.
+        Load local index from storage and return query engine.
         """
-        storage_dir = "src/.index_storage"
-
         # Check if index already exists
-        if os.path.exists(storage_dir):
+        if os.path.exists(self.storage_dir):
             logger.info("Loading existing index...")
-            storage_context = StorageContext.from_defaults(persist_dir=storage_dir)
+            storage_context = StorageContext.from_defaults(persist_dir=self.storage_dir)
             index = load_index_from_storage(storage_context)
         else:
             logger.info("Creating new index...")
-            documents = SimpleDirectoryReader("../data").load_data()
+            documents = SimpleDirectoryReader(self.data_dir).load_data()
             index = VectorStoreIndex.from_documents(documents)
-            index.storage_context.persist(persist_dir=storage_dir)
+            index.storage_context.persist(persist_dir=self.storage_dir)
 
-        return index
+        return index.as_query_engine()
 
 
 class LocalBaselineRAG(LocalRAG):
-    def __init__(self):
-        super().__init__()
-        self.query_engine = self.index.as_query_engine()
-
     def get_answer(self, question: str) -> str:
         return self.query_engine.query(question).response
 
 
 class LocalSubQuestionRAG(LocalRAG):
-    def __init__(self):
-        super().__init__()
-
+    def get_query_engine(self) -> any:
+        """
+        Get the SubQuestionQueryEngine to use for the RAG.
+        """
         # Create query engine tools
         query_engine_tools = [
             QueryEngineTool.from_defaults(
-                query_engine=self.index.as_query_engine(),
+                query_engine=self.query_engine,
                 name="document_search",
                 description="Công cụ này để tìm kiếm thông tin trong sách giáo khoa cấp trung học phổ thông.",
             )
@@ -98,3 +104,78 @@ class LocalSubQuestionRAG(LocalRAG):
 
     def get_answer(self, question: str) -> str:
         return self.query_engine.query(question).response
+
+
+class LocalReRankRAG(LocalRAG):
+    def __init__(
+        self,
+        storage_dir: str = "src/.index_storage",
+        data_dir: str = "../data",
+        bm25_dir: str = "src/.bm25_index_storage",
+        embed_similarity_top_k: int = 10,
+        bm25_similarity_top_k: int = 5,
+    ):
+        super().__init__(storage_dir, data_dir)
+        self.bm25_dir = bm25_dir
+        self.embed_similarity_top_k = embed_similarity_top_k
+        self.bm25_similarity_top_k = bm25_similarity_top_k
+
+    def _get_vector_retriever(self) -> any:
+        """
+        Get the vector-based retriever to use for the RAG.
+        """
+        # Get vector-based retriever
+        # Check if index already exists
+        if os.path.exists(self.storage_dir):
+            logger.info("Loading existing vector index...")
+            storage_context = StorageContext.from_defaults(persist_dir=self.storage_dir)
+            index = load_index_from_storage(storage_context)
+        else:
+            logger.info("Creating new vector index...")
+            documents = SimpleDirectoryReader(self.data_dir).load_data()
+            index = VectorStoreIndex.from_documents(documents)
+            index.storage_context.persist(persist_dir=self.storage_dir)
+
+        return index.as_retriever(similarity_top_k=self.embed_similarity_top_k)
+
+    def _get_bm25_retriever(self) -> any:
+        """
+        Get the BM25 retriever to use for the RAG.
+        """
+        if os.path.exists(self.bm25_dir):
+            bm25_retriever = BM25Retriever.from_persist_dir(
+                persist_dir=self.bm25_dir, similarity_top_k=self.embed_similarity_top_k
+            )
+        else:
+            # Create new BM25 retriever
+            documents = SimpleDirectoryReader(self.data_dir).load_data()
+            node_parser = SentenceSplitter(chunk_size=256, chunk_overlap=20)
+            nodes = node_parser.get_nodes_from_documents(documents, show_progress=True)
+            bm25_retriever = BM25Retriever.from_nodes(
+                nodes, similarity_top_k=self.embed_similarity_top_k
+            )
+            bm25_retriever.storage_context.persist(persist_dir=self.bm25_dir)
+
+        return bm25_retriever
+
+    def get_query_engine(self) -> any:
+        """
+        Get the query engine to use for the RAG.
+        """
+        # Get the vector and BM25 retriever
+        vector_retriever = self._get_vector_retriever()
+        bm25_retriever = self._get_bm25_retriever()
+
+        # Get hybrid retriever
+        hybrid_retriever = QueryFusionRetriever(
+            retrievers=[bm25_retriever, vector_retriever],
+            node_postprocessors=[
+                SimilarityPostprocessor(similarity_cutoff=0.7)
+            ],  # Reranker
+        )
+
+        query_engine = RetrieverQueryEngine.from_retriever(
+            retriever=hybrid_retriever, response_mode="compact"
+        )
+
+        return query_engine
